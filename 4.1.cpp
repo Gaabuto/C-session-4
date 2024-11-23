9cb19e8d18a299664d9ab005e4d2b645647714e0
Gaabuto <khanh040604@gmail.com>
2024-11-23 14:51:46 +0700

    4.1

---
+++ 4.1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int random;
+	printf("moi nhap 1 so nguyen bat ky: ");
+	scanf("%d", &random);
+	if(random>0){
+		printf("so nay la so duong");
+	}else if (random = 0){
+		printf("so nay la so 0");
+	}else {
+		printf("so nay la so am");
+	}
+ 	
+	return 0;
+}
